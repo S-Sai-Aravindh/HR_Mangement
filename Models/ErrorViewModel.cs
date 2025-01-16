939faeaf1098636136cd8776b7d05beb61f17e82
Sai Aravindh <saiaravindhchangepond27@gmail.com>
2025-01-16 21:32:23 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HR_Mangement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
